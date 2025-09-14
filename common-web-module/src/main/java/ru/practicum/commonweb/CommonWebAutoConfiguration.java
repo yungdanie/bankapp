eff376be610813a5bdf9e5750a521ffebfceeddb
dkocheshkov <d.kocheshkov@sel-be.ru>
2025-09-14 13:38:00 +0300

    zipkin: refactor WebClientFactory to use WebClient.Builder for tracing

--- common-web-module/src/main/java/ru/practicum/commonweb/CommonWebAutoConfiguration.java
+++ common-web-module/src/main/java/ru/practicum/commonweb/CommonWebAutoConfiguration.java
@@ -1,13 +1,15 @@
 package ru.practicum.commonweb;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.*;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager;
+import org.springframework.web.reactive.function.client.WebClient;
 import ru.practicum.commonweb.controller.ExceptionController;
-import ru.practicum.commonweb.factory.RestClientFactory;
 import ru.practicum.commonweb.factory.WebClientFactory;
 
 @AutoConfiguration
@@ -20,8 +22,11 @@ public class CommonWebAutoConfiguration {
         @Bean
         @ConditionalOnMissingBean(WebClientFactory.class)
         @ConditionalOnSingleCandidate(ReactiveOAuth2AuthorizedClientManager.class)
-        public WebClientFactory restClientFactory(ReactiveOAuth2AuthorizedClientManager reactiveOAuth2AuthorizedClientManager) {
-            return new WebClientFactory(reactiveOAuth2AuthorizedClientManager);
+        public WebClientFactory restClientFactory(
+                ReactiveOAuth2AuthorizedClientManager reactiveOAuth2AuthorizedClientManager,
+                WebClient.Builder webClientBuilder
+        ) {
+            return new WebClientFactory(reactiveOAuth2AuthorizedClientManager, webClientBuilder);
         }
 
     }
