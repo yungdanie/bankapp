d23ef0f4e7a51da35c872d4709b45089060a7dd5
dkocheshkov <d.kocheshkov@sel-be.ru>
2025-09-16 21:33:45 +0300

    prometheus-metrics: добавляем метрику неудачно отправленных ntf

--- notifications-module/src/main/java/ru/practicum/notifications/service/NotificationService.java
+++ notifications-module/src/main/java/ru/practicum/notifications/service/NotificationService.java
@@ -1,14 +1,26 @@
 package ru.practicum.notifications.service;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.practicum.common.Event;
+import ru.practicum.notifications.metrics.NotificationsMetrics;
 
+import java.util.Random;
 import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
+@AllArgsConstructor
 public class NotificationService {
+
+    private final NotificationsMetrics notificationsMetrics;
 
     public void processNotification(UUID uuid, Event event) {
         // do nothing
+
+        // рандомно отправляем метрики, что не смогли отправить метрики
+        if (ThreadLocalRandom.current().nextBoolean()) {
+            notificationsMetrics.onNotificationSendFailure(event);
+        }
     }
 }
